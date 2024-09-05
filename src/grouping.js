6b4e0cdedaa567d7907f1b2f350dc7b9c706cf13
unknown <kphanindravarma2005@gmail.com>
2024-09-05 20:55:52 +0530

    commited

--- src/grouping.js
+++ src/grouping.js
@@ -3,33 +3,43 @@ import { useState, useEffect } from "react";
 import { parseXMLFile } from "./parser";
 import Feed from "./feed";
 export default function Grouping() {
-  const [feeds, setFeeds] = useState([]);
+  const [feeds, setFeeds] = useState({});
   async function Main() {
     const jsonObj = await parseXMLFile(
       "https://raw.githubusercontent.com/ksubbu199/ksubbu199.github.io/gh-pages/feed.atom"
     );
     // setFeeds(jsonObj.feed.entry);
-    const info=jsonObj?.feed.entry
+    const info = jsonObj?.feed.entry;
     const result = Object.groupBy(info, ({ updated }) => updated);
-    setFeeds(result)
-    // console.log("length ",feeds["2024-09-03"].length)
+    setFeeds(result);
+
     // console.log(feeds)
   }
   useEffect(() => {
     Main();
   }, []);
 
-
-  console.log(feeds["2024-09-04"]?.length)
-  
+  console.log(feeds);
 
   return (
-    <>
-    <h1>heyyyy</h1>
-      {feeds["2024-09-04"]?.map((feed) => {
-        return <Feed {...feed} />;
+    <div>
+      <h1>h </h1>
+
+      {Object.values(feeds)?.map((info) => {
+        // debugger;
+
+        return (
+            <div className="info">
+            {info?.map((hey) => {
+          //   debugger;
+
+          return (
+          <div>
+          <Feed {...hey} />
+          </div>
+          )
+        })}</div>)
       })}
-    
-    </>
+    </div>
   );
 }
